79ef8a44659b960223311b2c4a26d9a13025bc80
Welinton Gomes Batista <133980158+welinton19@users.noreply.github.com>
2025-04-08 08:47:49 -0300

    deixando em arquitetura limpa
    criando uma arquitetura simples e criando interação com o mascote e adicionando o pacote automapper

---
+++ Interagir.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPokemon.Model
+{
+    public internal class Interagir
+    {
+        public int BrincarPokemon { get; private set; }
+        public int AlimentarPokemon { get; private set; }
+
+        public  void Interagir()
+        {
+            AlimentarPokemon--;
+            Console.WriteLine("Você alimentou o Pokemon!");
+            Console.WriteLine($"O Pokemon agora tem {AlimentarPokemon} de energia.");
+            Console.WriteLine("Você quer brincar com o Pokemon? (s/n)");
+            string resposta = Console.ReadLine();
+            if (resposta.ToLower() == "s")
+            {
+                BrincarPokemon++;
+                Console.WriteLine("Você brincou com o Pokemon!");
+                Console.WriteLine($"O Pokemon agora tem {BrincarPokemon} de energia.");
+            }
+            else
+            {
+                Console.WriteLine("Você não brincou com o Pokemon.");
+            }
+        }
+    }
+}
---
+++ MascoteApi.cs
@@ -0,0 +1,23 @@
+﻿using ApiPokemon.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPokemon
+{
+    public class MascoteApi
+    {
+        private List<AbilitiesClass>? abilities;
+        private int altura;
+        private int peso;
+        private string nome;
+
+        public override bool Equals(object? obj) => obj is Mascote mascote &&
+                   EqualityComparer<List<AbilitiesClass>>.Default.Equals(abilities, mascote.abilities) &&
+                   altura == mascote.altura &&
+                   peso == mascote.peso &&
+                   nome == mascote.nome;
+    }
+}
